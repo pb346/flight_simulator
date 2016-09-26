36e848c46ab984ef7f850af2bf0bbf4ab8260f38
paulbaba <paulbaba@buffalo.edu>
2016-09-26 08:57:22 -0700

    All axes are updating GUI

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -27,11 +27,18 @@ void MainWindow::onUpdateGUI(joystick_event* event)
 
 void MainWindow::updateValues(joystick_event* event)
 {
+    ui->xAxisValue->setText(QString::number(event->stick_x, 'f', 2 ));
+    ui->yAxisValue->setText(QString::number(event->stick_y, 'f', 2 ));
+    ui->zAxisValue->setText(QString::number(event->stick_z, 'f', 2 ));
     ui->throttleValue->setText(QString::number(event->throttle, 'f', 2 ));
+
 
 }
 void MainWindow::updateSliders(joystick_event* event)
 {
+    ui->xSlider->setSliderPosition(((event->stick_x + 1)/2) *100);
+    ui->ySlider->setSliderPosition(((event->stick_y + 1)/2) *100);
+    ui->zSlider->setSliderPosition(((event->stick_z + 1)/2) *100);
     ui->throttleSlider->setSliderPosition(((event->throttle + 1)/2) *100);
 }
 
