af8d01f13acfbf2b974ac4cd60b37aed5797894e
Evan Christensen <echriste@buffalo.edu>
2016-10-25 17:59:53 -0400

    Angular Orientation openGL Widget Working, still needs more functionality

--- src/glwidget.cpp
+++ src/glwidget.cpp
@@ -1,32 +1,30 @@
 #include "glwidget.h"
-#include <GL/glu.h>
-#include <GL/gl.h>
-#include <stdlib.h>
+#include <math.h>
 
 GLWidget::GLWidget(QWidget *parent) :
-    QGLWidget(parent)
+    QOpenGLWidget(parent)
 {
-
+    timer = new QTimer(this);
+    timer->start(400);
+    radius = 0.5;
+    angle = 0.0;
 }
 
 void GLWidget::initializeGL()
 {
-    initializeOpenGLFunctions();
-    glClearColor(0.2,0.2,0.2,1);
+    glClearColor(0,0,0,1);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHT0);
     glEnable(GL_COLOR_MATERIAL);
-    updateGL();
 }
 
 void GLWidget::paintGL()
 {
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    //glColor3f(1,0,0);
-    //gluSphere(0,20,20,20);
+    glColor3f(1,0,0);
 }
 
 void GLWidget::resizeGL(int w, int h)
 {
-
+    glViewport(0, 0, (GLint)w, (GLint)h);
 }
