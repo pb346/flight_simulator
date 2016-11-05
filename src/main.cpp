7da99431ab4fdc2169a2659b1877e7bcdf38c430
Paul Babala <paulbaba@buffalo.edu>
2016-11-04 20:54:07 -0400

    Removed test code from main

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     QTimer::singleShot(0, &w, SLOT(onEventLoopStarted()));
-/*******************************************************
-    QMainWindow *window = new QMainWindow();
-    window->setWindowTitle(QString("Testing with OpenGL"));
-    GLHeading* glheading = new GLHeading(window);
-    glheading->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    window->setCentralWidget(glheading);
-    window->setFixedSize(150,150);
-    window->show();
-Testing openGL functions ******************************/
     w.show();
 
     return a.exec();
